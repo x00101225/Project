639687eca38762c3d6e0ec3f1b5ace1766e6385c
James Hennessy <X00101225@ittd.ie>
2016-02-11 18:29:32 +0000

    feb

---
+++ Project2/Controllers/UploadController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Project2.Models;
+
+namespace Project2.Controllers
+{
+    public class UploadController : Controller
+    {
+        [HttpGet]
+        public ActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Upload(UploadModel u)
+        {
+            return View(u);
+        }
+    }
+}
